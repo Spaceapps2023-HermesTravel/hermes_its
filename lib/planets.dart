77b72a9667c4d85240bc103f58bcb4f593b6215b
TheLastGimbus <mateusz.soszynski@tuta.io>
2023-09-26 20:52:57 +0200

    better colors

--- lib/planets.dart
+++ lib/planets.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 
 enum Planet {
   // Thanx chat gpt!
-  mercury(size: 16, yearsToFullRotation: 0.2408467, color: Colors.grey),
-  venus(size: 20, yearsToFullRotation: 0.61519726, color: Colors.orangeAccent),
-  earth(size: 18, yearsToFullRotation: 1.0, color: Colors.green),
-  mars(size: 16, yearsToFullRotation: 1.8808158, color: Colors.orange),
-  jupiter(size: 28, yearsToFullRotation: 11.862615, color: Colors.amberAccent),
-  saturn(size: 26, yearsToFullRotation: 29.447498, color: Colors.yellow),
-  uranus(size: 22, yearsToFullRotation: 84.016846, color: Colors.blueGrey),
-  neptune(size: 24, yearsToFullRotation: 164.79132, color: Colors.blueAccent);
+  mercury(size: 16, yearsToFullRotation: 0.2408467, color: Color(0xFFA9A9A9)),
+  venus(size: 20, yearsToFullRotation: 0.61519726, color: Color(0xFFFFC300)),
+  earth(size: 18, yearsToFullRotation: 1.0, color: Color(0xFF007AC1)),
+  mars(size: 16, yearsToFullRotation: 1.8808158, color: Color(0xFFD14F32)),
+  jupiter(size: 28, yearsToFullRotation: 11.862615, color: Color(0xFFD9A667)),
+  saturn(size: 26, yearsToFullRotation: 29.447498, color: Color(0xFFE0C390)),
+  uranus(size: 22, yearsToFullRotation: 84.016846, color: Color(0xFF7EB8E2)),
+  neptune(size: 24, yearsToFullRotation: 164.79132, color: Color(0xFF455EC7));
 
   const Planet({
     required this.size,
