5121ae0c7cf6ed15b1972fdf8667a32628974d11
TheLastGimbus <mateusz.soszynski@tuta.io>
2023-09-26 20:56:47 +0200

    orbits

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -17,7 +17,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(title: const Text('ResHermes')),
       body: Container(
-        color: Colors.blueAccent,
+        color: Color(0xFF000418),
         child: StreamBuilder<double>(
           stream: Stream.periodic(
             const Duration(milliseconds: 10),
@@ -53,12 +53,15 @@ class Planets extends StatelessWidget {
         final size = constraints.biggest;
         final (centerX, centerY) = (size.width / 2, size.height / 2);
 
+        final maxRadius =
+            min(size.width, size.height) / 2 - Planet.values.last.size / 2;
+
+        radiusForPlanetIdx(idx) => maxRadius / Planet.values.length * (idx + 1);
+
         final planets = Planet.values.indexed.map<Widget>((e) {
           final (idx, planet) = e;
-          final maxRadius = min(size.width, size.height) / 2;
-          final radius = maxRadius / Planet.values.length * (idx + 1);
           final angle = (day / 365 / planet.yearsToFullRotation) * 2 * pi;
-          final (x, y) = calcPlanetPos(angle, radius);
+          final (x, y) = calcPlanetPos(angle, radiusForPlanetIdx(idx));
           return Positioned(
             left: centerX + (x - planet.size / 2),
             bottom: centerY + (y - planet.size / 2),
@@ -69,13 +72,34 @@ class Planets extends StatelessWidget {
             ),
           );
         });
+        // Add orbits for each planet
+        final orbits = Planet.values.indexed.map<Widget>((e) {
+          final (idx, _) = e;
+          final radius = radiusForPlanetIdx(idx);
+          return Positioned(
+            left: centerX - radius,
+            top: centerY - radius,
+            child: Container(
+              width: radius * 2,
+              height: radius * 2,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.grey.withOpacity(0.2),
+                  width: 0.5,
+                ),
+              ),
+            ),
+          );
+        });
         return Stack(
           children: [
             const Align(
               alignment: Alignment.center,
               child: Icon(Icons.sunny, size: 86, color: Colors.yellow),
             ),
-            ...planets
+            ...orbits,
+            ...planets,
           ],
         );
       },
